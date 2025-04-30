c1048a49d5a747fcedf4e864774265b622b7998b
JVCarv41 <jvc.04.off@gmail.com>
2025-04-30 17:45:09 -0300

    refactored the code

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const connectDB = require('../database/mongo');
+const authRoutes = require('../routes/authRoutes');
+const protectedRoutes = require('../routes/protectedRoutes');
+
+// Create express app
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Log when the app starts
+console.log('Starting Express server...');
+
+// Connect DB once when cold start
+connectDB()
+  .then(() => {
+    // Log when serverless handler is invoked
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+  });
+
+// Routes
+app.use('/api', authRoutes);
+app.use('/api', protectedRoutes);
+
+// Export serverless handler
+module.exports = app;
+module.exports.handler = serverless(app);
