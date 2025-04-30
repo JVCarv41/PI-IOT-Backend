2d03c11e6a4582e0808747dec65b80d0d0037ba3
JVCarv41 <jvc.04.off@gmail.com>
2025-04-30 16:21:39 -0300

    Added vercel.json

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET; // Use env var or fallback
+const JWT_SECRET = process.env.JWT_SECRET; // Use env var
 
 // REGISTRO
 const register = async (name, email, password) => {
