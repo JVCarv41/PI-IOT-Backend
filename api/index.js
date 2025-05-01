71826be0883ecdecf49f771d0388fed778676667
JVCarv41 <jvc.04.off@gmail.com>
2025-04-30 23:14:18 -0300

    added debug logs

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
+console.log("Antes")
 const serverless = require('serverless-http');
 const app = require('../src/app');
-
+console.log("Meio")
 module.exports = serverless(app);
+console.log("depois")
